92ca10da6c49ae7666043210d45af1a10e076aa2
jiahao.wu <1696764441@qq.com>
2023-11-08 21:55:47 +0800

    fix: 修复页面刷新Epic数据丢失问题

--- react-scrum/src/pages/Epic.jsx
+++ react-scrum/src/pages/Epic.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { setEpicModalShow } from '../redux/slice/epic';
 import CreateEpicModal from '../component/CreateEpicModal';
 import EpicList from '../component/EpicList';
+import { useParams } from 'react-router-dom';
+import { getProjectByIdAsync } from '../redux/slice/project';
 
 export default function Epic() {
 	const dispatch = useDispatch();
 
+	const params = useParams()
+	console.log("params", params)
+
 	const handleClickCreateEpic = () => {
 		dispatch(setEpicModalShow(true))
 	}
+
+	useEffect(()=> {
+		// 获取当前项目 并且保存到store，防止数据丢失
+		dispatch(getProjectByIdAsync(params.id))
+	},[params.id])
 
 	return (
 		<div className='epic-body'>
